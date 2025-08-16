172b7bb752cedae085ed5f38f5af7ab2f2b4d81b
623863423 <623863423@users.noreply.github.com>
2025-08-16 15:45:17 +0800

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-//草泥马  我啊实打实大
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+//草泥马  我啊实打实大cd D:\MyApplication
+//git commit -am "wip"
+//git push
